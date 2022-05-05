f21a48b140d9420dff7c804a3f2b14b01f837362
mrzhao <zhaomengru2018@gmail.com>
2022-05-05 17:07:56 +0800

    init

--- src/lib/api.js
+++ src/lib/api.js
@@ -19,16 +19,50 @@ export const createPostInfo = (node) => {
 }
 
 export const buildQuery = () => {
-  const queryObject = {}
+  const queryObject = {
+    query: `{
+   transactions(first: 100,
+     tags: [
+       {
+         name: "App-Name",
+         values: ["PublicSquare"]
+       },
+       {
+         name: "Content-Type",
+         values: ["text/plain"]
+       }
+     ]
+   ) {
+     edges {
+       node {
+         id
+         owner {
+           address
+         }
+         data {
+           size
+         }
+         block {
+           height
+           timestamp
+         }
+         tags {
+           name,
+           value
+         }
+       }
+     }
+   }
+ }`}
   return queryObject;
 }
 
 // in miliseconds
 var units = {
-  year  : 24 * 60 * 60 * 1000 * 365,
-  month : 24 * 60 * 60 * 1000 * 365/12,
-  day   : 24 * 60 * 60 * 1000,
-  hour  : 60 * 60 * 1000,
+  year: 24 * 60 * 60 * 1000 * 365,
+  month: 24 * 60 * 60 * 1000 * 365 / 12,
+  day: 24 * 60 * 60 * 1000,
+  hour: 60 * 60 * 1000,
   minute: 60 * 1000,
   second: 1000
 }
@@ -38,9 +72,9 @@ var rtf = new Intl.RelativeTimeFormat('en', { numeric: 'auto' })
 export const getRelativeTime = (ts1, ts2) => {
   var elapsed = ts1 - ts2
   // "Math.abs" accounts for both "past" & "future" scenarios
-  for (var u in units) 
-    if (Math.abs(elapsed) > units[u] || u === 'second') 
-      return rtf.format(Math.round(elapsed/units[u]), u)
+  for (var u in units)
+    if (Math.abs(elapsed) > units[u] || u === 'second')
+      return rtf.format(Math.round(elapsed / units[u]), u)
 }
 
 export const getPostTime = (timestamp) => {
@@ -53,9 +87,9 @@ export const getPostTime = (timestamp) => {
 export const abbreviateAddress = (address) => {
   if (!address)
     return address;
-  const firstFive = address.substring(0,5);
-  const lastFour = address.substring(address.length-4);
-  return `${firstFive}..${lastFour }`;
+  const firstFive = address.substring(0, 5);
+  const lastFour = address.substring(address.length - 4);
+  return `${firstFive}..${lastFour}`;
 }
 
 export const getTopicString = (input) => {
@@ -68,15 +102,15 @@ export const getTopicString = (input) => {
 }
 
 export const delay = (t) => {
-  return new Promise(function(resolve) {
-    setTimeout(function() {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
       resolve();
     }, t);
   });
 }
 
 export const delayResults = (milliseconds, results) => {
-  return delay(milliseconds).then(function() {
+  return delay(milliseconds).then(function () {
     return results;
   });
 }
